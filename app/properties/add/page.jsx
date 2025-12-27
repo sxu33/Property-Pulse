2481efd7eb75c7f0bcb1549e1a1357fb7740b74a
sxu33 <sxu3@academic.rrc.ca>
2025-12-27 14:14:44 -0600

    Created PropertyAddForm component

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,5 +1,16 @@
+import PropertyAddForm from "@/components/PropertyAddForm";
+import Property from "@/models/Property";
+
 const AddPropertyPage = () => {
-  return <div>Add Property Page</div>;
+  return (
+    <section class="bg-blue-50">
+      <div class="container m-auto max-w-2xl py-24">
+        <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddPropertyPage;
