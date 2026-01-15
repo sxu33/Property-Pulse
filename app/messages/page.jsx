ca694d61347e371ac9f6c9f304088a2021bbe087
sxu33 <sxu3@academic.rrc.ca>
2026-01-14 20:45:54 -0600

    created message page

---
+++ app/messages/page.jsx
@@ -0,0 +1,56 @@
+import connectDB from "@/config/database";
+import { getSessionUser } from "@/utils/getSessionUser";
+import Message from "@/models/Message";
+import "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToSerializableObject";
+
+const MessagePage = async () => {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw Error("No authorization");
+  }
+  const { userId } = sessionUser;
+
+  const [readMessages, unreadMessages] = await Promise.all([
+    Message.find({ recipient: userId, read: true })
+      .sort({ createdAt: -1 })
+      .populate("sender", "username")
+      .populate("property", "name")
+      .lean(),
+    Message.find({ recipient: userId, read: false })
+      .sort({ createdAt: -1 })
+      .populate("sender", "username")
+      .populate("property", "name")
+      .lean(),
+  ]);
+
+  const messages = [...unreadMessages, ...readMessages].map((messageDoc) => {
+    const message = convertToSerializableObject(messageDoc);
+    message.sender = convertToSerializableObject(messageDoc.sender);
+    message.property = convertToSerializableObject(messageDoc.property);
+    return message;
+  });
+
+  return (
+    <section className="bg-blue-50">
+      <div className="container m-auto py-24 max-w-6xl">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+
+          <div className="space-y-4">
+            {messages.length === 0 ? (
+              <p>You have no messages</p>
+            ) : (
+              messages.map((message) => (
+                <div key={message._id.toString()}>{message.name}</div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MessagePage;
